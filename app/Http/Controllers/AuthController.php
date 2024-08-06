9965b225735132a99edf7d9b71e7517b7a9f7bc5
manuel-tsvetanski <manuel.tsvetanski@gmail.com>
2024-08-06 10:12:15 +0300

    TK-02 | logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,6 +26,14 @@ class AuthController extends Controller
         return response()->json(['error' => 'Unauthorized'], 401);
     }
 
+    public function logout(Request $request)
+    {
+        // Revoke the token that was used to authenticate the current request
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+    
     public function register(Request $request)
     {
         $request->validate([
